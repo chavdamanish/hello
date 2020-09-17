4e5b9ee1e7707736e0d1d8cbdf7692f7b00acdaa
chavdamanish <chavdamanish1992@gmail.com>
2020-09-17 12:35:59 +0530

    First Commit

---
+++ app1/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'a.html', {'titles': 'Django', 'link': 'http://127.0.0.1:8000'})
+def profile(request):
+    return render(request, 'a.html', {'titles': 'Profile_Page', 'link': 'http://127.0.0.1:8000'})
+def expression(request):
+    a = int(request.POST['text1'])
+    b = int(request.POST['text2'])
+    c = a + 2 * b
+    return render(request, 'output.html', {'result': c})
